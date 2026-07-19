3d2e92aeabcaa038e0f508fa61695008bf575203
PiyushRt20 <piyushrawat4000@gmail.com>
2026-07-19 12:34:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (28.10%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        stack<int> st;
+        vector<int>freq(26, 0);
+        for(auto it : s){
+            freq[it- 'a']++;
+        }
+        vector<bool>inStack(26, false);
+        for(int i =0; i<s.size(); i++){
+            int val = s[i] - 'a';
+            if(inStack[val] == false){
+                while(!st.empty() && val <= st.top() && freq[st.top()] > 0){
+                    inStack[st.top()] = false;
+                    st.pop();
+                }
+                st.push(val);
+                inStack[val] = true;
+                freq[val]--;
+            }
+            else{
+                freq[val]--;
+            }
+        }
+        string t = "";
+        while(!st.empty()){
+            int val = st.top();
+            t += char(val + 'a');
+            st.pop();
+        }
+        reverse(t.begin(), t.end());
+        return t;
+    }
+};

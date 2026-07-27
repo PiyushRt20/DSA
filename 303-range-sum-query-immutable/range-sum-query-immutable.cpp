e33d5ecedcdc79a3c678e5e788170b6e95a6d442
PiyushRt20 <piyushrawat4000@gmail.com>
2026-07-27 22:15:47 +0530

    Time: 1462 ms (5.41%) | Memory: 330.6 MB (25.60%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -1,20 +1,45 @@
-class NumArray {
-    vector<int> preSum;
+class SGTree{
+    vector<int>seg;
 public:
-    NumArray(vector<int>& nums) {
-        int n = nums.size();
-        preSum.resize(n);
-        preSum[0] = nums[0];
-        for(int i =1; i<n; i++){
-            preSum[i] = preSum[i-1] + nums[i];
-        }
+    SGTree(int n){
+        seg.resize(4 * n +1);
     }
     
-    int sumRange(int left, int right) {
-        if(left == 0){
-            return preSum[right];
+    void build(int idx, int low, int high, vector<int>& nums){
+        if(low == high){
+            seg[idx] = nums[low];
+            return;
         }
-        return preSum[right] - preSum[left -1];
+        int mid = (low + high)/2;
+        build(2 * idx+1, low, mid, nums);
+        build(2 * idx+2, mid+1, high, nums);
+        seg[idx] = seg[2*idx+1] + seg[2*idx+2];
+    }
+    int query(int idx, int low, int high, int l, int r){
+        if(l > high || r < low){
+            return 0;
+        }
+        if(l <= low && r >= high ){
+            return seg[idx];
+        }
+        int mid = (low+high)/2;
+        int left = query(2*idx+1, low, mid, l, r);
+        int right = query(2*idx+2, mid+1, high, l, r);
+        return left + right;
+    }
+};
+class NumArray {
+    vector<int> arr;
+    int n;
+public:
+    NumArray(vector<int>& nums) {
+        n = nums.size();
+        arr = nums;
+    }
+    int sumRange(int left, int right) {
+        SGTree s(n);
+        s.build(0, 0, n-1, arr);
+        return s.query(0, 0, n-1, left, right);
     }
 };
 

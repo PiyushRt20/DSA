5a95d481598c1b380338c40a54fc5ee6a4a51808
PiyushRt20 <piyushrawat4000@gmail.com>
2026-07-29 18:01:08 +0530

    Time: 12 ms (86.96%) | Memory: 16.8 MB (84.06%) - LeetSync

---
+++ 3813-smallest-palindromic-rearrangement-ii/smallest-palindromic-rearrangement-ii.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    long long nCr(int n, int r, int k){
+        r = min(r, n-r);
+        long long result = 1;
+        for(int i =1; i<= r; i++){
+            result = result * (n - r + i) / i;
+            if(result >= k){
+                return k;
+            }
+        }
+        return result;
+    }
+    string smallestPalindrome(string s, int k) {
+        int n = s.size();
+        string ans = "";
+        int cntLetters = 0;
+        vector<int> count(26, 0);
+        for(int i =0; i<n/2; i++){
+            count[s[i] - 'a']++;
+            cntLetters++;
+        }
+        long long tways = 1;
+        for(int i =0; i<26; i++){
+            if(count[i] > 0){
+                tways *= nCr(cntLetters, count[i], k);
+                cntLetters -= count[i];
+            }
+            if(tways >= k){
+                break;
+            }
+        }
+        if(tways < k){
+            return "";
+        }
+
+        for(int i =0; i<n/2; i++){
+            for(int j = 0; j<26; j++){
+                if(count[j] > 0){
+                    count[j]--;
+                    long long ways = 1;
+                    int totalLetters = 0;
+                    for(int c =0; c<26; c++){
+                        totalLetters += count[c];
+                    }
+                    for(int c =0; c<26; c++){
+                        if(count[c] > 0){
+                            ways *= nCr(totalLetters, count[c], k); 
+                            totalLetters -= count[c];
+                        }
+
+                        if(ways >= k) break;
+                    }
+                    if(ways >= k){
+                        ans.push_back(j + 'a');
+                        break;
+                    }
+                    k -= ways;
+                    count[j]++;
+                }
+            }
+        }
+        string r = ans;
+        reverse(r.begin(), r.end());
+        if(n % 2 != 0){
+            ans += s[n/2];
+        }
+        ans += r;
+        return ans;
+    }
+};

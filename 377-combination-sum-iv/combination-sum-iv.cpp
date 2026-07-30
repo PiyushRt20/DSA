89397b08af2c8fdf9bafc8faa284de9ce3f91981
PiyushRt20 <piyushrawat4000@gmail.com>
2026-07-30 20:54:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (51.40%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int solve(vector<int>&nums, int tar, vector<int>&dp){
-        if(tar < 0 ) return 0;
+    int solve(int tar, vector<int>& nums, vector<int>& dp){
+        if(tar < 0) return 0;
         if(tar == 0) return 1;
         if(dp[tar] != -1){
             return dp[tar];
         }
-
         int cnt = 0;
         for(auto it : nums){
-            cnt += solve(nums, tar - it, dp);
+            cnt += solve(tar - it , nums, dp);
         }
         return dp[tar] = cnt;
     }
     int combinationSum4(vector<int>& nums, int target) {
         vector<int> dp(target+1, -1);
-        return solve(nums, target, dp);
+        return solve(target, nums, dp);
     }
 };

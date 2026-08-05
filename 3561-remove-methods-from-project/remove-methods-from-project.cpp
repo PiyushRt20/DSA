7a8d704d193b1c4309a487d243c2353095cc5c92
PiyushRt20 <piyushrawat4000@gmail.com>
2026-08-05 18:43:32 +0530

    Time: 331 ms (37.59%) | Memory: 345.9 MB (28.62%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.cpp
+++ 3561-remove-methods-from-project/remove-methods-from-project.cpp
@@ -2,14 +2,10 @@ class Solution {
 public:
     void dfs(int node, vector<vector<int>> &adj, vector<int>& vis, vector<int>& indegree){
         vis[node] = 1;
-        // indegree[node]--;
         for(auto it : adj[node]){
+            indegree[it]--;
             if(!vis[it]){
-                indegree[it]--;
                 dfs(it, adj, vis, indegree);
-            }
-            else{
-                indegree[it]--;
             }
         }
     }
@@ -24,9 +20,9 @@ public:
         dfs(k, adj, vis, indegree);
         vector<int> ans;
         bool flag = false;
-        for(auto it : indegree){
-            cout<<it<<" ";
-        }
+        // for(auto it : indegree){
+        //     cout<<it<<" ";
+        // }
         for(int i =0; i<n; i++){
             if(vis[i] == 1 && indegree[i] > 0){
                 flag = true;

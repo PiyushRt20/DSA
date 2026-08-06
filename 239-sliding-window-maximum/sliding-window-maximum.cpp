cfdb44eea084e1eabdc12fe73843a7fc034a7430
PiyushRt20 <piyushrawat4000@gmail.com>
2026-08-07 01:02:08 +0530

    Time: 28 ms (37.64%) | Memory: 139.3 MB (47.29%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -5,14 +5,18 @@ public:
         vector<int> ans;
         deque<int> dq;
         for(int i =0; i<n; i++){
-            while(!dq.empty() && dq.front() <= i-k){
+            //maintaining correct window
+            if(!dq.empty() && dq.front() <= i-k){
                 dq.pop_front();
             }
             // to make it monotonic decreasing
             while(!dq.empty() && nums[dq.back()] < nums[i]){
                 dq.pop_back();
             }
+            // storing idx to dq
             dq.push_back(i);
+
+            //pushing it to ans
             if(i >= k-1){
                 ans.push_back(nums[dq.front()]);
             }

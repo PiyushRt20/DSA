1cdb9f500ff32a25b592b78c3980b462d5d07131
PiyushRt20 <piyushrawat4000@gmail.com>
2026-08-10 19:19:18 +0530

    Time: 43 ms (57.44%) | Memory: 18.1 MB (34.32%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -8,7 +8,7 @@ public:
             return dp[n];
         }
         for(int i =1; i*i <= n; i++){
-            if(willAliceWin(n - i*i, dp) == false){
+            if(willAliceWin(n - i*i, dp) == false){         //ab bob ki chance h aur agr vo harta h toh alice will win hum bs alice ki turn krke bob ke liye call kr denge baki recursion apne ap krega
                 return dp[n] =  true;
             }
         }

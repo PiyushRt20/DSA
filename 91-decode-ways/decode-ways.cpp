1d9bba9f551f7f0278172de77165bbab3caad84d
PiyushRt20 <piyushrawat4000@gmail.com>
2026-08-22 02:09:22 +0530

    Time: 3 ms (16.09%) | Memory: 8.5 MB (88.12%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int solve(int idx, string &s, int n, vector<int>& dp){
-        if(idx > n){
-            return 0;
+        if(idx == n){
+            return 1;
         }
-        if(idx == n) return 1;
         if(dp[idx] != -1){
             return dp[idx];
         }
-        if(s[idx] == '0') return 0;
+        if(s[idx] == '0'){ 
+            return dp[idx] = 0;
+        }    
         int one = solve(idx+1, s, n, dp);
         int two = 0;
-        if(s[idx] < '2'){
+        if(idx+1 < n && s[idx] < '2'){
             two = solve(idx+2, s, n, dp);
         }
         else if(s[idx] == '2' && idx+1 < n && s[idx+1] < '7'){

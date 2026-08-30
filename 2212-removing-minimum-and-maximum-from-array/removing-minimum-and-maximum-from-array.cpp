1b32041bea07fd971589510ff5414d6d3aaf8e67
PiyushRt20 <piyushrawat4000@gmail.com>
2026-08-30 13:36:35 +0530

    Time: 2 ms (42.34%) | Memory: 92.2 MB (22.14%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -11,74 +11,24 @@ public:
             maxi = max(maxi, nums[i]);
             mini = min(mini, nums[i]);
         }
+        int l_Idx = -1;
+        int u_Idx = -1;
+        for(int i = 0; i<n; i++){
+            if(nums[i] == mini){
+                l_Idx = i;
+            }
+        }
+        for(int i = 0; i<n; i++){
+            if(nums[i] == maxi){
+                u_Idx = i;
+            }
+        }
         int minSteps = 1e9;
-        int cnt = 0;
-        int i = 0;
-        int j = n-1;
-        bool one = false;
-        bool two = false;
-        while(i < j){
-            if(!one && (nums[i] == mini || nums[i] == maxi)){
-                one = true;
-                cnt++;
-            }
-            if(!two && (nums[j] == mini || nums[j] == maxi)){
-                two = true;
-                cnt++;
-            }
-            if(one && two){
-                break;
-            }
-            if(one){
-                j--;
-                cnt++;
-            }
-            else if(two){
-                i++;
-                cnt++;
-            }
-            else{
-                i++;
-                j--;
-                cnt += 2;
-            }
-        }
-        minSteps = min(minSteps, cnt);
-        cnt = 0;
-        one = false;
-        two = false;
-        for(int i =0; i<n; i++){
-            cnt++;
-            if(nums[i] == maxi || nums[i] == mini){
-                if(one) two = true;
-                one = true;
-                
-            }
-            // if(nums[i] == maxi || nums[i] == mini){
-            //     two = true;
-            // }
-            if(one && two ){
-                break;
-            }
-        }
-        minSteps = min(minSteps, cnt);
-        cnt = 0;
-        one = false;
-        two = false;
-        for(int i =n-1; i>=0; i--){
-            cnt++;
-            if(nums[i] == maxi || nums[i] == mini){
-                if(one) two = true;
-                one = true;
-            }
-            // if(nums[i] == maxi || nums[i] == mini){
-            //     two = true;
-            // }
-            if(one && two ){
-                break;
-            }
-        }
-        minSteps = min(minSteps, cnt);
+        minSteps = min(minSteps, max(l_Idx, u_Idx) + 1);
+        minSteps = min(minSteps, n - min(l_Idx, u_Idx));
+        minSteps = min(minSteps, l_Idx + 1 + n - u_Idx);
+        minSteps = min(minSteps, n - l_Idx  + u_Idx + 1);
         return minSteps;
+
     }
 };

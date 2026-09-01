20981d30d8375da6237dce4800dc117150679d2a
PiyushRt20 <piyushrawat4000@gmail.com>
2026-09-01 11:36:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (41.51%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -8,12 +8,10 @@ public:
         for(int i =1; i<n; i++){
             prefix[i] = prefix[i-1] * nums[i-1];
         }
-        suffix[n-1] = 1;
-        for(int i =n-2; i>=0; i--){
-            suffix[i] = suffix[i+1] * nums[i+1];
-        }
-        for(int i =0; i<n; i++){
-            prefix[i] = prefix[i] * suffix[i];
+        int suf = 1;
+        for(int i =n-2; i>= 0; i--){
+            suf *= nums[i+1];
+            prefix[i] *= suf ;
         }
         return prefix;
     }
